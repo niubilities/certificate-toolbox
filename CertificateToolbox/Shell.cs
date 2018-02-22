2138497d481adc5056839e6704041c98754b2966
mihai <stancu_meehigh@yahoo.com>
2018-02-22 13:50:41 +0100

    Add empty UI project

---
+++ CertificateToolbox/Shell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CertificateToolbox
+{
+    public partial class Shell : Form
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}
