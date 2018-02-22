eb5e5b17fcda540a5e90b65eb6c2461acaaf5ec4
mihai <stancu_meehigh@yahoo.com>
2018-02-22 18:01:52 +0100

    Allow generating as many certs in a chain as we want

--- CertificateToolbox/Shell.Designer.cs
+++ CertificateToolbox/Shell.Designer.cs
@@ -29,53 +29,51 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.certificateDetails3 = new CertificateToolbox.CertificateDetails();
-            this.certificateDetails2 = new CertificateToolbox.CertificateDetails();
-            this.certificateDetails1 = new CertificateToolbox.CertificateDetails();
+            this.layout = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1111, 535);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1161, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 101);
             this.button1.TabIndex = 66;
             this.button1.Text = "Save";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // certificateDetails3
+            // layout
             // 
-            this.certificateDetails3.Issuer = this.certificateDetails2;
-            this.certificateDetails3.Location = new System.Drawing.Point(677, 14);
-            this.certificateDetails3.Name = "certificateDetails3";
-            this.certificateDetails3.Size = new System.Drawing.Size(292, 235);
-            this.certificateDetails3.TabIndex = 67;
+            this.layout.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.layout.AutoScroll = true;
+            this.layout.Location = new System.Drawing.Point(12, 12);
+            this.layout.Name = "layout";
+            this.layout.Size = new System.Drawing.Size(1121, 581);
+            this.layout.TabIndex = 68;
+            this.layout.WrapContents = false;
             // 
-            // certificateDetails2
+            // button2
             // 
-            this.certificateDetails2.Issuer = this.certificateDetails1;
-            this.certificateDetails2.Location = new System.Drawing.Point(350, 14);
-            this.certificateDetails2.Name = "certificateDetails2";
-            this.certificateDetails2.Size = new System.Drawing.Size(292, 235);
-            this.certificateDetails2.TabIndex = 67;
-            // 
-            // certificateDetails1
-            // 
-            this.certificateDetails1.Issuer = null;
-            this.certificateDetails1.Location = new System.Drawing.Point(22, 14);
-            this.certificateDetails1.Name = "certificateDetails1";
-            this.certificateDetails1.Size = new System.Drawing.Size(292, 235);
-            this.certificateDetails1.TabIndex = 67;
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(1163, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 65);
+            this.button2.TabIndex = 69;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Shell
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1248, 605);
-            this.Controls.Add(this.certificateDetails3);
-            this.Controls.Add(this.certificateDetails2);
-            this.Controls.Add(this.certificateDetails1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.layout);
             this.Controls.Add(this.button1);
             this.Name = "Shell";
             this.Text = "Form1";
@@ -85,9 +83,8 @@
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private CertificateDetails certificateDetails1;
-        private CertificateDetails certificateDetails2;
-        private CertificateDetails certificateDetails3;
+        private System.Windows.Forms.FlowLayoutPanel layout;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- CertificateToolbox/Shell.cs
+++ CertificateToolbox/Shell.cs
@@ -2,6 +2,7 @@
 using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
 using Org.BouncyCastle.Math;
+using System.Linq;
 
 namespace CertificateToolbox
 {
@@ -12,13 +13,21 @@ namespace CertificateToolbox
             InitializeComponent();
         }
 
+        public CertificateDetails LastCert => layout.Controls.Cast<CertificateDetails>().LastOrDefault();
+
         private void button1_Click(object sender, EventArgs e)
         {
             button1.Enabled = false;
 
-            certificateDetails3.Generate();
+            LastCert.Generate();
 
             button1.Enabled = true;
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var newCert = new CertificateDetails {Issuer = LastCert};
+            layout.Controls.Add(newCert);
         }
     }
 }
