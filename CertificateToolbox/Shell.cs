74eb6481690376a5ae7e1438fe950fb8b2e0c425
mihai <stancu_meehigh@yahoo.com>
2018-02-22 15:08:57 +0100

    Genreate CA from UI

--- CertificateToolbox/Shell.cs
+++ CertificateToolbox/Shell.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows.Forms;
+using CreateCertificate;
 
 namespace CertificateToolbox
 {
@@ -15,6 +10,31 @@ namespace CertificateToolbox
         public Shell()
         {
             InitializeComponent();
+
+            not_before.Value = DateTime.UtcNow.AddDays(-1);
+            not_after.Value = DateTime.UtcNow.AddYears(100);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+
+            var generator = new Generator
+            {
+                SubjectName = subject.Text,
+                NotBefore = not_before.Value,
+                NotAfter = not_after.Value,
+                IsCertificateAuthority = true
+            };
+
+            var certificate = generator.Generate();
+
+            var store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(certificate);
+            store.Close();
+
+            button1.Enabled = true;
         }
     }
 }
